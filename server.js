1cef9e2276fd522e89d5b8cf2f2e9bfc20715e92
kpingul <kirckpingul@gmail.com>
2015-11-21 14:19:05 -0800

    updated: added testingnode using mocha

--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express'),
+	request = require('supertest'),
 	port    = process.env.PORT || 3000,
 	app     = express();
 
+//Static files
 app.use(express.static(__dirname + '/client'));
+
+//EJS
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/server/views');
+
+//Routes
+
+app.get('/', function(req , res) {
+
+	res.render('index');
+});
+
+app.get('/about', function(req, res) {
+	res.render('about');
+});
 
 app.listen(port, function() {
 	console.log('listening on port ' + port);
